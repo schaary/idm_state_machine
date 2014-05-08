099e4003e2431a65b1310e86aa85367828927c6f
Michael Schaarschmidt <michael@schaaryworks.com>
2014-05-08 10:46:44 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IdmStateMachine::Application.config.secret_key_base = 'a43ef1c8c4a53f2575e4832345bacfd9844b3485f6fc92f0f3074ed7ef61bf35f57176455cf02e19790e576b47b49e3a8d9025f63075eb2505a1299819f3f874'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IdmStateMachine::Application.config.session_store :cookie_store, key: '_idm_state_machine_session'
